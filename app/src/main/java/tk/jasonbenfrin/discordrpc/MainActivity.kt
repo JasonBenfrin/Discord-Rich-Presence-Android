e4639ba4ab8028d08dc12319fa9760802c595784
JasonBenfrin <bhonemyatmin123@gmail.com>
2022-06-01 03:48:47 +0630

    Activities and Fragments styles added. Save, Status, Rich Presence and Load are still needed.

--- app/src/main/java/tk/jasonbenfrin/discordrpc/MainActivity.kt
+++ app/src/main/java/tk/jasonbenfrin/discordrpc/MainActivity.kt
@@ -1,32 +1,41 @@
 package tk.jasonbenfrin.discordrpc
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import android.webkit.WebView
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigationrail.NavigationRailView
 
 class MainActivity : AppCompatActivity() {
+    private var previousFragment : Int? = null
+    private var previousOrientation : Int = Configuration.ORIENTATION_UNDEFINED
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
+        previousOrientation = resources.configuration.orientation
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .setReorderingAllowed(true)
-                    // TODO: Change to set Presence tab
-                .add(R.id.fragmentContainerView, LoginFragment())
+                .add(R.id.fragmentContainerView, PresenceFragment())
                 .commit()
         }
 
-        findViewById<BottomNavigationView>(R.id.bottomBar).setOnItemSelectedListener { item -> itemSelectedListener(item) }
+        val bottomBar : BottomNavigationView = findViewById(R.id.bottomBar)
+        bottomBar.selectedItemId = R.id.set
+        bottomBar.setOnItemSelectedListener { item -> itemSelectedListener(item) }
         findViewById<NavigationRailView>(R.id.navRail).setOnItemSelectedListener{ item -> itemSelectedListener(item) }
     }
 
@@ -36,11 +45,9 @@ class MainActivity : AppCompatActivity() {
         super.onConfigurationChanged(newConfig)
         val bottomBar : BottomNavigationView = findViewById(R.id.bottomBar)
         val navRail : NavigationRailView = findViewById(R.id.navRail)
-        val previousFragment : Fragment? = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
         setContentView(R.layout.activity_main)
         val newBottomBar : BottomNavigationView = findViewById(R.id.bottomBar)
         val newNavRail : NavigationRailView = findViewById(R.id.navRail)
-
         when (newConfig.orientation) {
             Configuration.ORIENTATION_LANDSCAPE -> {
                 newNavRail.setOnItemSelectedListener{ item -> itemSelectedListener(item) }
@@ -63,17 +70,22 @@ class MainActivity : AppCompatActivity() {
                 newBottomBar.selectedItemId = bottomBar.selectedItemId
             }
         }
+        previousOrientation = newConfig.orientation
     }
 
     private fun itemSelectedListener( item : MenuItem ) : Boolean {
         val fragmentManager = supportFragmentManager.beginTransaction()
             .setReorderingAllowed(true)
             .setCustomAnimations(R.anim.fragment_enter, R.anim.fragment_exit, R.anim.fragment_enter, R.anim.fragment_exit)
+        if( previousFragment != null && previousFragment == item.itemId && previousOrientation == resources.configuration.orientation) return false
         when (item.itemId) {
             R.id.login -> fragmentManager.replace(R.id.fragmentContainerView, LoginFragment())
-            R.id.connect -> fragmentManager.replace(R.id.fragmentContainerView, ConnectFragment())
+            R.id.set -> fragmentManager.replace(R.id.fragmentContainerView, PresenceFragment())
+            R.id.store -> fragmentManager.replace(R.id.fragmentContainerView, Save())
+            R.id.about -> fragmentManager.replace(R.id.fragmentContainerView, Info())
             else -> return false
         }
+        previousFragment = item.itemId
         fragmentManager.commit()
         return true
     }
@@ -84,17 +96,43 @@ class MainActivity : AppCompatActivity() {
             return inflater.inflate(R.layout.login, container,false)
         }
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
             // TODO: set WebView visibility to VISIBLE when token is not found
         }
     }
 
-    class ConnectFragment : Fragment() {
+    class PresenceFragment : Fragment() {
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
             super.onCreateView(inflater, container, savedInstanceState)
-            return inflater.inflate(R.layout.connect, container,false)
+            return inflater.inflate(R.layout.presence, container,false)
         }
     }
+
+    class Save : Fragment() {
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
+            super.onCreateView(inflater, container, savedInstanceState)
+            return inflater.inflate(R.layout.save, container,false)
+        }
+    }
+
+    class Info : Fragment() {
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
+            super.onCreateView(inflater, container, savedInstanceState)
+            return inflater.inflate(R.layout.info, container,false)
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            val githubButton : ImageButton = view.findViewById(R.id.github)
+            githubButton.setOnClickListener{ v -> github() }
+        }
+
+        private fun github() {
+            startActivity(Intent("android.intent.action.VIEW", Uri.parse("https://github.com/JasonBenfrin/Discord-Rich-Presence-Android")))
+        }
+    }
+
+    // UI: TabLayout and ViewPager2 Synchronise
+
 }
